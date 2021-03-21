85bfe6b9be3e110e112255e8bbcd4071bd7dc022
Muhammad Hamza <muhammadhamzam1486@gmail.com>
2021-03-21 14:50:06 +0500

    CSS complete

---
+++ index.js
@@ -0,0 +1,11 @@
+/////////////////////////////////////
+// Getting Elements
+const inputTodo = document.querySelector(".header__input");
+const btnAdd = document.querySelector(".header__add-btn");
+
+/////////////////////////////////////
+// Functions
+
+btnAdd.addEventListener("click", function (e) {
+  e.preventDefault();
+});
